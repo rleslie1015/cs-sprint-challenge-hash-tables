e0df60a20fa4fb837bf6d6eba3ddb3bfad192564
Leslie Rodriguez <rleslie1015@gmail.com>
2020-09-11 21:05:53 -0500

    completes ext1 passing all tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,53 @@
+"""
+Understand
+    weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+                   |       |
+                    \     /
+                       21  # output is [3, 1] 
+
+    weights = [ 9 ], length = 1, limit = 9
+                # output is None
+
+
+P
+
+   create a dictionary 
+   loop through list for each item check if dictionary contains limit- weight at each loop 
+        set markers for both the weight and (limit-weight)
+        
+        if (limit - weight) is in dictionary then return tuple
+        if not place weight and index into dictionary
+        (placing weight as key and index as value)
+
+
+E
+R
+"""
+
+
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
+    hashtable = {}
 
+    for i in range(length):
+        key = weights[i]
+        weight2find = limit - weights[i]
+
+        if weight2find in hashtable:
+            weight1 = hashtable[weight2find]
+            tup = (i, weight1) 
+            return tup
+        else: 
+            hashtable[weights[i]] = i
+            # print(hashtable[weights[i]])
     return None
+
+
+
+
+weightlist = [ 4, 6, 10, 15, 16 ]
+print(get_indices_of_item_weights(weightlist, 5, 21))
